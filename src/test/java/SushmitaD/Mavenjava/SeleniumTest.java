2bd045af01cafa9a8f3e67cb31780e4015b8536e
Sushmita <sushmitad@zimetrics.com>
2021-12-08 14:23:32 +0530

    demo1

---
+++ src/test/java/SushmitaD/Mavenjava/SeleniumTest.java
@@ -0,0 +1,30 @@
+package SushmitaD.Mavenjava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		
+		System.out.println("BrowserAutomation");
+
+		
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		
+		System.out.println("elementsUi");
+
+		
+	}
+
+	
+	
+}
