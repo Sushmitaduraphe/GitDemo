be308e64930fbc1f03576aff15bc96fb669508eb
Sushmita <sushmitad@zimetrics.com>
2021-12-08 10:36:56 +0530

    first commit

---
+++ src/test/java/SushmitaD/Mavenjava/AppiumTest.java
@@ -0,0 +1,27 @@
+package SushmitaD.Mavenjava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+//import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		
+		System.out.println("NativeAppAndroid");
+		
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		
+		System.out.println("IOSApps");
+
+		
+	}
+
+}
